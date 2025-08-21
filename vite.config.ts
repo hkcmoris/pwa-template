f25124bc204c9747b9951cbd20dbe0ae83073bf0
hkcmoris <43518120+hkcmoris@users.noreply.github.com>
2025-08-21 23:37:37 +0200

    fix: add minimal index.html for dev server

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
     build: {
         outDir: "public/assets",
         emptyOutDir: true,
         rollupOptions: {
+            input: resolve(__dirname, "src/main.ts"),
             output: {
                 entryFileNames: "[name]-[hash].js",
                 chunkFileNames: "[name]-[hash].js",
