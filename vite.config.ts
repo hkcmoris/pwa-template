34b995257df12e03e05a6f35002180c28a81693b
hkcmoris <43518120+hkcmoris@users.noreply.github.com>
2025-08-21 21:06:37 +0200

    fix indentation

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  build: {
-    outDir: 'public/assets',
-    emptyOutDir: true,
-    rollupOptions: {
-      output: {
-        entryFileNames: '[name]-[hash].js',
-        chunkFileNames: '[name]-[hash].js',
-        assetFileNames: '[name]-[hash][extname]'
-      }
-    }
-  }
+    build: {
+        outDir: "public/assets",
+        emptyOutDir: true,
+        rollupOptions: {
+            output: {
+                entryFileNames: "[name]-[hash].js",
+                chunkFileNames: "[name]-[hash].js",
+                assetFileNames: "[name]-[hash][extname]",
+            },
+        },
+    },
 });
