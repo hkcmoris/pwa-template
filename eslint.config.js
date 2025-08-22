79e784a2187fc5218c3901473344235a0547c24f
hkcmoris <43518120+hkcmoris@users.noreply.github.com>
2025-08-22 08:57:20 +0200

    build: add flat eslint config

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+const js = require('@eslint/js');
+const tsPlugin = require('@typescript-eslint/eslint-plugin');
+const tsParser = require('@typescript-eslint/parser');
+const globals = require('globals');
+const eslintConfigPrettier = require('eslint-config-prettier');
+
+module.exports = [
+  {
+    ignores: ['dist/**', 'public/assets/**']
+  },
+  js.configs.recommended,
+  {
+    files: ['src/**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+      },
+      globals: globals.browser
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules
+    }
+  },
+  eslintConfigPrettier
+];
