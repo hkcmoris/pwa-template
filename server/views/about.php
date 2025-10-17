51b9aaef33783fc48833095c1af37304b55a4051
Ondřej Moravec <moravec@devground.cz>
2025-10-17 09:09:54 +0200

    update about text content

--- server/views/about.php
+++ server/views/about.php
@@ -1,9 +1,8 @@
 <div data-island="about">
     <h1>O aplikaci</h1>
     <p>
-        Tato šablona ukazuje progresivní webovou aplikaci postavenou na
-        serverem renderovaných PHP šablonách s navigací vylepšenou pomocí htmx.
-        TypeScript „ostrovy“ jsou bundlované přes Vite a načítají se na vyžádání,
-        aby byla první návštěva co nejmenší a nejrychlejší.
+        Konfigurátor pro sestavení vozidla a nástavby Hagemann<br>
+        Verze aplikace: <?= htmlspecialchars($appVersion) ?><br>
+        2025 Hagemann a.s.
     </p>
 </div>
