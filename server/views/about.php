63c7c36364b9c15523ef38fd2e4c05db81a15a3b
Ondřej Moravec <moravec@devground.cz>
2025-10-17 10:18:23 +0200

    fix wrong variable for displaying app version

--- server/views/about.php
+++ server/views/about.php
@@ -2,7 +2,7 @@
     <h1>O aplikaci</h1>
     <p>
         Konfigurátor pro sestavení vozidla a nástavby Hagemann<br>
-        Verze aplikace: <?= htmlspecialchars($appVersion) ?><br>
+        Verze aplikace: <?= htmlspecialchars(APP_VERSION, ENT_QUOTES, 'UTF-8') ?><br>
         2025 Hagemann a.s.
     </p>
 </div>
